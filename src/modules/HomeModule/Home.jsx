00b55eee233b6c467ca7900c1ef00e2d10b835e5
hamo.alpatel@gmail.com <hamo.alpatel@gmail.com>
2024-05-13 13:00:48 +0300

    rashad

--- src/modules/HomeModule/Home.jsx
+++ src/modules/HomeModule/Home.jsx
@@ -3,19 +3,25 @@ import React from 'react'
 import heroImg from "../../assets/image/hero.png";
 import serviceImg from '../../assets/GIf/2.gif'
 import HeroSection from '../SharedModule/HeroSection/components/HeroSection';
-// Import Swiper styles
+// Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
+// import useM 
+// Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/pagination';
-import 'swiper/css/navigation';
 
-import { Keyboard, Pagination, Navigation } from 'swiper/modules';
+// import './styles.css';
+
+// import required modules
+import { Pagination } from 'swiper/modules';
+
 import './css/home.css'
 import ServicesCard from '../SharedModule/ServicesCard/ServicesCard';
 import Features from '../SharedModule/Features/Features';
+import Blog from '../SharedModule/BlogCard/Blog';
 const Home = () => {
   return (
-    <div>
+    <div className=''>
       <HeroSection
         title={"حقق نموك مع نظامنا المتكامل للتسويق "}
         btnText={"اشتري الان"}
@@ -25,9 +31,9 @@ const Home = () => {
         }
       />
       {/* features */}
-      <Features 
-      headTitle={'اهم ما يميزنا ؟ '}
-      headDesc={'نُوفر فريق جودة، إدارة، ومتابعة لكل مشروع مع العميل، مع إضافة نظام متكامل للمشروع لتسليمه بعد الانتهاء'}
+      <Features
+        headTitle={'اهم ما يميزنا ؟ '}
+        headDesc={'نُوفر فريق جودة، إدارة، ومتابعة لكل مشروع مع العميل، مع إضافة نظام متكامل للمشروع لتسليمه بعد الانتهاء'}
       />
       {/* services */}
       <div className="bg">
@@ -100,6 +106,45 @@ const Home = () => {
           </div>
         </div>
       </div>
+
+
+      <div className="swiper-body container">
+       <div className="head">
+        <h1>أحدث المقالات </h1>
+        <p>استمتع بقراءة افضل و احدث المقالات حول التحول الرقمي و تاثيره في سوق العمل و زيادة الارباح و قصص و نجلح الشركات </p>
+       </div>
+        <Swiper
+      // slidesPerView={3}
+        breakpoints={{
+          600: {
+            slidesPerView: 2,
+          },
+          
+          960: {
+            slidesPerView: 3,
+          },
+        }}
+          // slidesPerView={3}
+          spaceBetween={50}
+          pagination={{
+            dynamicBullets: true,
+          }}
+          modules={[Pagination]}
+          className="mySwiper"
+        >
+          <SwiperSlide> <Blog /> </SwiperSlide>
+          <SwiperSlide> <Blog /> </SwiperSlide>
+          <SwiperSlide> <Blog /> </SwiperSlide>
+          <SwiperSlide> <Blog /> </SwiperSlide>
+          <SwiperSlide> <Blog /> </SwiperSlide>
+          <SwiperSlide> <Blog /> </SwiperSlide>
+          <SwiperSlide> <Blog /> </SwiperSlide>
+          <SwiperSlide> <Blog /> </SwiperSlide>
+          <SwiperSlide> <Blog /> </SwiperSlide>
+        </Swiper>
+      </div>
+
+
 
 
     </div>
