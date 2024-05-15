3233d2ca0c24d77b6dbd1e40966104117147c1c4
hamo.alpatel@gmail.com <hamo.alpatel@gmail.com>
2024-05-15 23:49:41 +0300

    rashod

--- src/modules/HomeModule/components/TechContainer/TechContainer.jsx
+++ src/modules/HomeModule/components/TechContainer/TechContainer.jsx
@@ -18,19 +18,19 @@ const TechContainer = () => {
       className=" grid-cols-1 items-start md:grid-cols-2 gap-4 grid md:mt-14">
           <div className="m-auto techItem w-full">
             <h4 id="techTitle">التصميم</h4>
-            <img className="md:max-w-[450px] max-w-[250px]" src={Tech_1} alt="" />
+            <img className="md:max-w-[450px] max-w-[280px]" src={Tech_1} alt="" />
           </div>
           <div className="m-auto techItem w-full">
             <h4 id="techTitle">التصميم</h4>
-            <img  className="md:max-w-[450px] max-w-[250px]" src={Tech_1} alt="" />
+            <img  className="md:max-w-[450px] max-w-[280px]" src={Tech_1} alt="" />
           </div>
           <div className="m-auto techItem w-full">
             <h4 id="techTitle">التصميم</h4>
-            <img className="md:max-w-[450px] max-w-[250px]" src={Tech_1} alt="" />
+            <img className="md:max-w-[450px] max-w-[280px]" src={Tech_1} alt="" />
           </div>
           <div className="m-auto techItem w-full">
             <h4 id="techTitle">التصميم</h4>
-            <img className="md:max-w-[450px] max-w-[250px]" src={Tech_1} alt="" />
+            <img className="md:max-w-[450px] max-w-[280px]" src={Tech_1} alt="" />
           </div>
       </div>
     </div>
