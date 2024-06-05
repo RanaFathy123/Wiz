90a14d2b4f4f0edf153abbbec3d0e1a9e152e2f6
hamo.alpatel@gmail.com <hamo.alpatel@gmail.com>
2024-06-05 09:50:10 +0300

    rashad

--- src/modules/SharedModule/components/HeroSection/components/HeroSection.jsx
+++ src/modules/SharedModule/components/HeroSection/components/HeroSection.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './style.css'
 import star from '../../../../../assets/image/star.png';
 
-const HeroSection = ({ title, descrption, image, btnText, animationText, animationDesc }) => {
+const HeroSection = ({ title, descrption, image, btnText, animationText, animationDesc ,titleClass}) => {
   return (
     <div className=" heroSection container mt-10">
       <img src={star} className='stars' />
@@ -10,7 +10,7 @@ const HeroSection = ({ title, descrption, image, btnText, animationText, animati
       <img src={star} className='stars' />
       <img src={star} className='stars' />
       <div className="text">
-        <h1 data-aos={animationText} className="title">{title}</h1>
+        <h1 data-aos={animationText} className={`title ${titleClass}`}>{title}</h1>
         <p data-aos={animationDesc} className="desc">{descrption}</p>
         <a href="" className="btn-heroSection">
           {btnText}
--- src/modules/about-us/About.jsx
+++ src/modules/about-us/About.jsx
@@ -3,6 +3,7 @@ import aboutUs from '../../assets/GIf/aboutUs.gif'
 import Accordion from "./components/Acordion/Accordion"
 import Bannar from "../SharedModule/Bannar/Bannar"
 import lam from '../../assets/image/companies_logos/whoLambezz.png'
+import Map from '../../assets/GIf/Map.gif'
 import Features from "../SharedModule/Features/Features"
 // aos
 import AOS from 'aos';
@@ -33,6 +34,7 @@ const About = () => {
           "نُوفر فريق جودة، إدارة، ومتابعة لكل مشروع مع العميل، مع إضافة نظام متكامل للمشروع لتسليمه بعد الانتهاء"
         }
       />
+
       {/* start edu */}
 
       <section className="education container overflow-y-hidden" id="education">
@@ -101,24 +103,52 @@ const About = () => {
         </div>
       </section>
       {/* end edu */}
-     <div className="bg-[#F5F5F5F5]">
+      <div className="bg-[#F5F5F5F5]">
 
-      <div className="trust container">
-      <h1 className="md:text-[45px] text-[32px] max-w-[600px] text-[#0065d2] font-bold">موثوق بنا من قبل أفضل الشركات</h1> 
-      <div className="comp grid md:grid-cols-6  grid-cols-3">
-        <img src={lam} alt="" />
-        <img src={lam} alt="" />
-        <img src={lam} alt="" />
-        <img src={lam} alt="" />
-        <img src={lam} alt="" />
-        <img src={lam} alt="" />
+        <div className="trust container">
+          <h1 className="md:text-[45px] text-[32px] max-w-[600px] text-[#0065d2] font-bold">موثوق بنا من قبل أفضل الشركات</h1>
+          <div className="comp grid md:grid-cols-6  grid-cols-3">
+            <img src={lam} alt="" />
+            <img src={lam} alt="" />
+            <img src={lam} alt="" />
+            <img src={lam} alt="" />
+            <img src={lam} alt="" />
+            <img src={lam} alt="" />
+          </div>
+        </div>
+
       </div>
+      {/* --map-- */}
+      <HeroSection
+        animationText={'fade-left'}
+        animationDesc={'fade-up'}
+        title={'متواجدين  في كل مكان'}
+        titleClass={'text-[#0065D2]'}
+        btnText={'اطلب استشارتك الان'}
+        image={Map}
+        descrption={'نقدم خدماتنا في أكثر من دولة حول العالم اكشتف الأن'}
+      />
+      {/* end trust */}
+      <div className="container">
+        <h2 className="heading text-right mr-0"> نضمن لك نمو تجاري مُستدام من خلال منظومة شاملة وفريق مختص لدعمك في كل خطوة.</h2>
+        <div className="futures mt-5">
+          <div data-aos={'fade-up'} className="fut">
+            <i className="fa-regular fa-credit-card"></i>
+            <h1>وسائل الدفع</h1>
+            <p>نُوفر لك جميع وسائل الدفع المتاحة في العالم، لضمان تجربة دفع سهلة ومريحة</p>
+          </div>
+          <div data-aos={'fade-up'} className="fut">
+            <i className="fa-solid fa-handshake"></i>
+            <h1>فريق عمل محترف</h1>
+            <p>لدينا أكثر من 1000 موظف في جميع المجالات المختصة لضمان نجاح مشروعك بالكامل </p>
+          </div>
+          <div data-aos={'fade-up'} className="fut">
+            <i className="fa-solid fa-money-bill-transfer"></i>
+            <h1>ضمان المعاملات المالية</h1>
+            <p>يحق لعميلنا دفع فقط 20 % من ميزانية المشروع والباقي على دفعات بعد استلام كل جزء من العمل، مما يجعلنا الأفضل من الناحية المالية.</p>
+          </div>
+        </div>
       </div>
-
-     </div>
-
-
-
 
 
 
