d4417cc33e4134f3d6b31c63dd4ac8cab6246be9
hamo.alpatel@gmail.com <hamo.alpatel@gmail.com>
2024-06-08 09:53:58 +0300

    rashad

--- src/modules/ServiceDetails/ServiceDetails.jsx
+++ src/modules/ServiceDetails/ServiceDetails.jsx
@@ -22,7 +22,7 @@ export default function ServiceDetails() {
   // -----------animation libarary----------------
   useEffect(() => {
     Aos.init({
-      duration: 2000,
+      duration: 5000,
       offset:200,
     });
   }, [])
